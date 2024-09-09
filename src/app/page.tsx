c8545cb277987c31e4cd987112b10fee94c23ec8
AlexKat <katsubo79@gmail.com>
2024-09-09 14:24:47 +0300

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <div className="container">
-        <h1>Home Page sdfsd</h1>
+        <h1>Home Page</h1>
         <div className="cont-right"></div>
         {/* <div className="cont-decor "></div> */}
       </div>
